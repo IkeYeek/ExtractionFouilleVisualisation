4c908c7d30880c0ffc286ae4e40237679bd0b108
Luk <luk@ikeloud>
2025-11-26 15:10:15 +0100

    feat: one shell script to download both the .xls that contains var description and all the data

--- dl_dataset.sh
+++ dl_dataset.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
+set -e
+
+OUTPUT_DIR="./dataset"
 
 BASE_URL="https://open-data-assurance-maladie.ameli.fr/medicaments/download.php?Dir_Rep=Open_MEDIC_Base_Complete&Annee="
 DOWNLOAD_BASE="https://open-data-assurance-maladie.ameli.fr/medicaments/"
 YEARS=($(seq 2014 2024))
-OUTPUT_DIR="./dataset"
+TMP_DIR="$OUTPUT_DIR/tmp"
 
-rm -rf $OUTPUT_DIR
-mkdir $OUTPUT_DIR
+mkdir -p "$TMP_DIR"
+
+curl -L "https://www.assurance-maladie.ameli.fr/sites/default/files/2024_descriptif-variables_open-medic.xls" -o "$OUTPUT_DIR/var_desc.xls"
 
 for year in "${YEARS[@]}"; do
   URL="$BASE_URL$year"
-  echo $URL
   FILE_PATH=$(wget -q -O - "$URL" | sed -n 's/.*href="\([^"]*\)".*/\1/p')
-  wget -P "$OUTPUT_DIR" "$DOWNLOAD_BASE$FILE_PATH"
-  gunzip "$OUTPUT_DIR"/*.gz
-  mv "$OUTPUT_DIR"/*.csv "$OUTPUT_DIR/ds_$year.csv"
+
+  if [ -z "$FILE_PATH" ]; then
+    echo "Error: Could not extract file path for year $year"
+    exit 1
+  fi
+
+  if [[ $FILE_PATH == *.zip ]]; then
+    curl -L "$DOWNLOAD_BASE$FILE_PATH" -o "$TMP_DIR/$year.zip" || exit 1
+    unzip -d "$TMP_DIR" "$TMP_DIR/$year.zip" || exit 1
+    rm "$TMP_DIR/$year.zip"
+    mv "$TMP_DIR"/* "$OUTPUT_DIR/OPEN_MEDIC_$year.csv"
+  else
+    curl -L "$DOWNLOAD_BASE$FILE_PATH" -o "$TMP_DIR/$year.gz" || exit 1
+    gunzip "$TMP_DIR/$year.gz" || exit 1
+    mv "$TMP_DIR"/* "$OUTPUT_DIR/OPEN_MEDIC_$year.csv"
+  fi
+  sleep 30 # desperate attempt to fight rate limits...
 done
--- dl_variables_description.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-wget https://www.assurance-maladie.ameli.fr/sites/default/files/2024_descriptif-variables_open-medic.xls
