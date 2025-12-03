922c87739c738b5ad70f0aa6be8abd4be0564f05
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2025-12-03 12:10:07 +0100

    fix: correct (and cleaner) graphs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+pd_all_rows_ctx = lambda: pd.option_context('display.max_rows', None)
+plt.subplots_adjust(left=0.3) # leave space on the left ...
+
 # variables = pd.read_excel("2024_descriptif-variables_open-medic.xls")
 # print(variables)
 col_to_label = {
@@ -14,7 +17,6 @@ col_to_label = {
     "GEN_NUM": "Groupe Générique",
     "age": "Age",
     "sexe": "Sexe",
-    "Bénéficiaire": "Bénéficiaire",
     "BEN_REG": "Région de Résidence du Bénéficiaire",
     "PSP_SPE": "Prescripteur",
     "REM": "Montant Remboursé",
@@ -47,48 +49,49 @@ df = df[~df["BEN_REG"].isin([0, 9, 99])] # drop Unknown regions
 # print(df.groupby(["BEN_REG", "age"])["REM"].count().groupby(level=0).idxmax().map(lambda x: x[1])) # groupe avec le + de remboursements
 
 
-# https://www.insee.fr/fr/statistiques/2012713#tableau-TCRD_004_tab1_regions2016
-region_to_pop_2025 = {
-    5: 2_253_657, # 68605616 - 66351959
-    11: 12_450_849,
-    24: 2_581_479,
-    27: 2_793_080,
-    28: 3_341_312,
-    32: 5_973_933, # Hauts-de-France
-    44: 5_544_051, # Grand Est
-    52: 3_936_719,
-    53: 3_475_895,
-    75: 6_191_209,
-    76: 6_201_587,
-    84: 8_260_096,
-    93: 5_241_587 + 360_162 # Provence-Alpes-Côte d'Azur + Corse
-}
+from population_stats import total_people_per_region as region_to_pop_2025
+from population_stats import old_people_percent_per_region, total_people_per_age_per_region
 
 def normalize_by_pop(v: pd.Series):
-    region, reimbursed = v.name, v.iloc[0]
+    region_code: int = v.name
+    reimbursed: float = v.iloc[0]
 
-    if region in region_to_pop_2025:
-        reimbursed /= region_to_pop_2025[region]
+    if region_code in region_to_pop_2025:
+        reimbursed /= region_to_pop_2025[region_code]
     else:
-        print(f"warn: {region} not in region_to_pop_2025")
+        print(f"warn: {region_code} not in region_to_pop_2025")
 
     return pd.Series(data=[reimbursed], index=["REM"])
 
 # Plot montant remboursé euros par habitant VS region
+
 # reg_to_reimbursement: pd.Series = df.groupby(by="BEN_REG")["REM"].sum()
 # reg_to_reimbursement_normalized = reg_to_reimbursement.to_frame().apply(normalize_by_pop, axis=1) # normalize py pop
+# reg_to_reimbursement_normalized["old_people_percent"] = reg_to_reimbursement_normalized.index.map(old_people_percent_per_region) * 10 # TODO: fix this scale ..
 # reg_to_reimbursement_normalized.index = reg_to_reimbursement_normalized.index.map(region_map)
-# reg_to_reimbursement_normalized.sort_values(by="REM", ascending=True).plot.barh(ylabel="Montant remboursé euros par habitant", legend=False)
-# plt.show()
+# reg_to_reimbursement_normalized_sorted = reg_to_reimbursement_normalized.sort_values(by="REM", ascending=True)
+# reg_to_reimbursement_normalized_sorted.plot.barh(ylabel="Montant remboursé euros par habitant", legend=False, title="Montant remboursé euros par habitant par région INSEE")
+
 
 # Plot montant remboursé euros par (habitant: chaque tranche d'age stackée) VS region (stacked)
-reg_to_reimbursement: pd.Series = df.groupby(by=["BEN_REG", "age"])["REM"].sum()
-reg_to_reimbursement_normalized = reg_to_reimbursement.to_frame().apply(lambda s: s.iloc[0] / region_to_pop_2025[s.name[0]], axis=1).reset_index(name="reimbursed_per_person") # normalize py pop (douteux puisque nombre(vieux) != nombre(jeunes) dans chaque région mais bon..)
-pivot = reg_to_reimbursement_normalized.pivot(index="BEN_REG", columns="age", values="reimbursed_per_person")
-pivot = pivot[[0, 20, 60]]
-sorted_pivot = pivot.iloc[pivot.sum(axis=1).reset_index()[0].sort_values(ascending=True).index] # ugly sort
-sorted_pivot.index = sorted_pivot.index.map(region_map) # map INSEE region code to region name
-sorted_pivot.columns = sorted_pivot.columns.map(age_map) # map to age groups names
-sorted_pivot.plot.barh(stacked=True, ylabel="Montant remboursé euros par habitant", xlabel="", title="Montant remboursé euros par habitant(et tranche d'age) par region")
-plt.subplots_adjust(left=0.3)
+
+# reg_to_reimbursement: pd.Series = df.groupby(by=["BEN_REG", "age"])["REM"].sum()
+# reg_to_reimbursement_normalized = reg_to_reimbursement.to_frame().apply(lambda s: s.iloc[0] / region_to_pop_2025[s.name[0]], axis=1).reset_index(name="reimbursed_per_person") # normalize py pop (douteux puisque nombre(vieux) != nombre(jeunes) dans chaque région mais bon..)
+# pivot = reg_to_reimbursement_normalized.pivot(index="BEN_REG", columns="age", values="reimbursed_per_person")
+# pivot = pivot[[0, 20, 60]]
+# sorted_pivot = pivot.iloc[pivot.sum(axis=1).reset_index()[0].sort_values(ascending=True).index] # ugly sort
+# sorted_pivot.index = sorted_pivot.index.map(region_map) # map INSEE region code to region name
+# sorted_pivot.columns = sorted_pivot.columns.map(age_map) # map to age groups names
+# sorted_pivot.plot.barh(stacked=True, ylabel="Montant remboursé euros par habitant", xlabel="", title="Montant remboursé euros par habitant(et tranche d'age) par region")
+
+
+# The french's preffered medication
+
+# df.groupby(by=["L_ATC2"])["REM"].sum().sort_values(ascending=True).iloc[-10:].plot.barh(ylabel="Sous-Groupe Thérapeutique", xlabel="Montant total remboursé euros", title="Les médicaments coutant le plus cher à la sécu")
+
+# df.groupby(by=["L_ATC5"])["BOITES"].sum().sort_values(ascending=True).iloc[-20:].plot.barh(ylabel="Sous-Groupe Substance Chimique", xlabel="Nombre de boites remboursées", title="Les catégories de médicaments préférées des français", logx=True)
+
+
+
+
 plt.show()
